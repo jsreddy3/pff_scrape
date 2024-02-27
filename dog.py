e224609bd8c0fa7e5d7075f4b1f59171479553cf
Jaiden Reddy <jaidenreddy@gmail.com>
2024-02-26 19:44:15 -0800

    Delete dog.py

--- dog.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-# res = requests.get('https://jaidenreddy.substack.com/p/an-essay-for-writing-class')
-# res = requests.get('https://www.pff.com/nfl/players/jared-goff/10635')
-# res = requests.get('https://explorecourses.stanford.edu/search;jsessionid=a507pwykn2a1irzgygqoft1o?view=catalog&academicYear=&page=0&q=OCEANS&filter-departmentcode-OCEANS=on&filter-coursestatus-Active=on&filter-term-Spring=on')
-print(res.text)
-
